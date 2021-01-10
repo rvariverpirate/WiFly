5514cea6eb9f9707e31fdaacca99bf5d8794d222
rvariverpirate <cannellajs2@vcu.edu>
2021-01-10 03:48:02 -0500

    Creating heriarchical folder layout, WebCam still working

--- WiFlyDrone/src/Sensors/IMU.h
+++ WiFlyDrone/src/Sensors/IMU.h
@@ -20,7 +20,7 @@ void setupIMU(){
     // Setup Alternative I2C
     I2Ctwo.begin(SDA2,SCL2, 400000);
 
-    // IMU Begin Breaks things
+    // Start IMU
     status = IMU.begin();
     if (status < 0) {
         Serial.println("IMU initialization unsuccessful");
@@ -31,4 +31,39 @@ void setupIMU(){
     else{
         Serial.println("IMU Checks out!");
     }
+
+    // setting the accelerometer full scale range to +/-8G 
+    IMU.setAccelRange(MPU9250::ACCEL_RANGE_8G);
+    // setting the gyroscope full scale range to +/-500 deg/s
+    IMU.setGyroRange(MPU9250::GYRO_RANGE_500DPS);
+    // setting DLPF bandwidth to 20 Hz
+    IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ);
+    // setting SRD to 19 for a 50 Hz update rate
+    IMU.setSrd(19);
+}
+
+void printIMU(){
+    // Read IMU Data
+    IMU.readSensor();
+
+    // Display IMU Data
+    Serial.print(IMU.getAccelX_mss(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getAccelY_mss(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getAccelZ_mss(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getGyroX_rads(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getGyroY_rads(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getGyroZ_rads(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getMagX_uT(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getMagY_uT(),6);
+    Serial.print("\t");
+    Serial.print(IMU.getMagZ_uT(),6);
+    Serial.print("\t");
+    Serial.println(IMU.getTemperature_C(),6);
 }
